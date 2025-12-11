0cd5e7707f1e1ac19b5d432afcfde8be3d662aaa
Death Stalker <jawadvamps@gmail.com>
2025-12-11 23:55:39 +0330

    second train for nlp traning

--- basics/tokenization/tokenizer.py
+++ basics/tokenization/tokenizer.py
@@ -22,12 +22,22 @@
 
 #week 1 - train  1
 import re
+
 def simple_tokenize(text):
     text = text.lower()
     text = re.sub(r'[^\w\s]', "", text)
     new_list = text.split()
+    stopwords = ["the", "is", "in", "of", "and", "to"]
+    
+    i = 0
+    while i < len(new_list):
+        if new_list[i] in stopwords:
+            new_list.pop(i)  # Use pop to remove the element at index i
+        else:
+            i += 1  # Only increment if no removal occurs
+    
     return new_list
 
 if __name__ == "__main__":
-    sample_text = "why this code is not working?"
+    sample_text = "this is a test text"
     print(simple_tokenize(sample_text))
